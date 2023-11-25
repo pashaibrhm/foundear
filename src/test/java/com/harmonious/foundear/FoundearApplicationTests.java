a0d866d9205279c71835a77d975c50fe9f7a8c3f
Pasha Ibrahim <ariefpasa.ibrahim@gmail.com>
2023-11-25 11:06:58 +0700

    Adding Some Dependency and Properties

--- src/test/java/com/harmonious/foundear/FoundearApplicationTests.java
+++ src/test/java/com/harmonious/foundear/FoundearApplicationTests.java
@@ -2,12 +2,23 @@ package com.harmonious.foundear;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FoundearApplicationTests {
 
+	ApplicationModules modules = ApplicationModules.of(FoundearApplication.class);
+
 	@Test
 	void contextLoads() {
+		assertThat(modules).isNotNull();
+	}
+
+	@Test
+	void shouldBeCompliant() {
+		modules.verify();
 	}
 
 }
