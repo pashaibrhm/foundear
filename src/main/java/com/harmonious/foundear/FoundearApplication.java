aefbc436a49801011f32f94cf08a73ee01c30eb7
Pasha Ibrahim <arief.ibrahim@indivaragroup.com>
2023-11-25 08:47:51 +0700

    Base Commit

---
+++ src/main/java/com/harmonious/foundear/FoundearApplication.java
@@ -0,0 +1,13 @@
+package com.harmonious.foundear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoundearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harmonious/foundear/FoundearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmonious.foundear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
