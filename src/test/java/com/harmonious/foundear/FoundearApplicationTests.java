5bbe60359d09a76d424929788c29c6b630c6d44c
Pasha Ibrahim <ariefpasa.ibrahim@gmail.com>
2023-12-10 19:48:20 +0700

    Reset

--- src/test/java/com/harmonious/foundear/FoundearApplicationTests.java
+++ src/test/java/com/harmonious/foundear/FoundearApplicationTests.java
@@ -1,10 +1,6 @@
 package com.harmonious.foundear;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.modulith.core.ApplicationModules;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FoundearApplicationTests {
