ba7e9c6cec89034b8fcdfaafc1c27f4b6dd93804
Pasha Ibrahim <ariefpasa.ibrahim@gmail.com>
2024-03-03 20:01:05 +0700

    change database and rearrange import User entity

--- repository/src/main/java/com/harmonious/foundear/repository/user/UserRepository.java
+++ repository/src/main/java/com/harmonious/foundear/repository/user/UserRepository.java
@@ -1,9 +1,10 @@
 package com.harmonious.foundear.repository.user;
 
+import com.harmonious.foundear.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<com.harmonious.foundear.entity.user.User, UUID>, JpaSpecificationExecutor<com.harmonious.foundear.entity.user.User> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 }
